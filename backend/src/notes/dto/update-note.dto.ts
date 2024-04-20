a2f51cc31455dcbf0c7906565e5c191bf13e542d
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-04-20 11:53:40 -0300

    remove files

--- backend/src/notes/dto/update-note.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNoteDto } from './create-note.dto';
-
-export class UpdateNoteDto extends PartialType(CreateNoteDto) {
-  id: number;
-}
--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd frontend && npm i && npm run dev & cd backend && docker-compose -f docker-compose.yml up -d && npm i && npm run start:dev
